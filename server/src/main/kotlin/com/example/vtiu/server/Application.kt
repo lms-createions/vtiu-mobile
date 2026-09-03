9bfc781dfacbf231ad083581fe6ad86ee3a4d462
lydiatds03 <lydiatds03@gmail.com>
2026-09-03 19:12:47 +0000

    Fix: Correct CallLogging typo and finalize database imports

--- server/src/main/kotlin/com/example/vtiu/server/Application.kt
+++ server/src/main/kotlin/com/example/vtiu/server/Application.kt
@@ -8,7 +8,7 @@ import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.calllogging.*
 import com.example.vtiu.server.db.DatabaseFactory
 
 fun main() {
